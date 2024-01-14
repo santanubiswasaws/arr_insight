d3722230207d75d8d89ce137002df1c9d1a3dda0
santanubiswas <santanubiswas@yahoo.com>
2024-01-13 19:08:24 -0800

    css changes

--- arr_lib/styling.py
+++ arr_lib/styling.py
@@ -22,3 +22,47 @@ BUTTON_STYLE="""
     }
                                     
     </style>"""
+
+
+GLOBAL_STYLING = """
+.title:after{
+    content: 'TEST';
+    }
+
+.title{
+    content: 'TEST';
+    }
+.StatusWidget-enter{
+    visibility: hidden;
+}
+
+.StatusWidget-exit{
+    visibility: hidden;
+}
+
+.StatusWidget-exit-done{
+    visibility: hidden;
+}
+
+
+.stStatusWidget{
+    visibility: hidden;
+}
+.stStatusWidget>div{
+    visibility: hidden;
+}
+.stStatusWidget>div>img{
+    visibility: hidden;
+}
+.stStatusWidget>div>label{
+    visibility: hidden;
+}
+.stStatusWidget>div>span{
+    visibility: hidden;
+}
+
+.stToolbar{
+    visibility: None;
+}
+
+"""
